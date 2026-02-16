f7278190c8455e5c1a81337abd9d24c8aeab7556
Ansony Romero <aromero@cardinalsystems.com.ar>
2026-02-16 09:53:55 -0400

    Agregar archivos de proyecto.

---
+++ ManejoDeCadenasCaracteres/Program.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections.Specialized;
+using System.Text;
+
+StringCollection misStrings = new StringCollection();
+
+// 2. Agregar elementos
+// Cada string es UN OBJETO más en el HEAP
+misStrings.Add("Goku");
+misStrings.Add("Vegeta");
+misStrings.Add("Trunks");
+
+Console.WriteLine(misStrings[0]);
+
+foreach (String? s in misStrings) 
+{ 
+    Console.WriteLine(s);
+}
+
+var conLinq = misStrings.Cast<string>().Where(s => s.StartsWith("G"));
+
+
+StringBuilder sb = new StringBuilder();
+
+sb.Append('-',6);
+sb.Append(" Mundo");
+
+
+sb.AppendLine();
+
+sb.Replace('-', '*');
+
+
+
+
+
+foreach (char c in sb.ToString())
+{
+    Console.Write(c);
+}
+
